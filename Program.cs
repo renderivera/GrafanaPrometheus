098f8fccc416a7589a6278fef70b2bfc9cf27562
renderivera <31817478+renderivera@users.noreply.github.com>
2023-11-08 10:49:56 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using Prometheus;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseMetricServer(); // expose Prometheus Metrics Endpoint
+app.UseHttpMetrics(options=> {
+    options.AddCustomLabel("host", context => context.Request.Host.Host);
+}); //adding metrics related to HTTP
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
